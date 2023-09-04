cc6d5770de5026f167f432641ba33968069ef3d7
khakestari <mostafanmz@yahoo.com>
2023-09-04 21:43:10 +0330

    allow users to pick a location on the map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -15,11 +15,29 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Map'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+                onPressed: _pickedLocation == null
+                    ? null
+                    : () {
+                        Navigator.of(context).pop(_pickedLocation);
+                      },
+                icon: const Icon(Icons.check))
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -29,6 +47,15 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  position: _pickedLocation!,
+                ),
+              },
       ),
     );
   }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 import '../helpers/location_helper.dart';
@@ -29,7 +30,7 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final selectedLocation = await Navigator.of(context).push<LatLng>(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (ctx) => MapScreen(
@@ -40,7 +41,7 @@ class _LocationInputState extends State<LocationInput> {
     if (selectedLocation == null) {
       return;
     }
-    //..
+    print(selectedLocation.latitude);
   }
 
   @override
